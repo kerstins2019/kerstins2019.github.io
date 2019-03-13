d565a4e73e2502ec1c5c9a9ff76ea1a31256e0dc
kerstins2019 <48378821+kerstins2019@users.noreply.github.com>
2019-03-13 21:54:30 +0100

    1 commit Los casos
    La carpeta entera de 2019.13.03_LosCasos

---
+++ LosCasos/scripts/config_xml.js
@@ -0,0 +1,50 @@
+var TSC = TSC || {};
+
+TSC.embedded_config_xml = '<x:xmpmeta tsc:version="2.0.1" xmlns:x="adobe:ns:meta/" xmlns:tsc="http://www.techsmith.com/xmp/tsc/">\
+   <rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:xmp="http://ns.adobe.com/xap/1.0/" xmlns:xmpDM="http://ns.adobe.com/xmp/1.0/DynamicMedia/" xmlns:xmpG="http://ns.adobe.com/xap/1.0/g/" xmlns:xmpMM="http://ns.adobe.com/xap/1.0/mm/" xmlns:tscDM="http://www.techsmith.com/xmp/tscDM/" xmlns:tscIQ="http://www.techsmith.com/xmp/tscIQ/" xmlns:tscHS="http://www.techsmith.com/xmp/tscHS/" xmlns:stDim="http://ns.adobe.com/xap/1.0/sType/Dimensions#" xmlns:stFnt="http://ns.adobe.com/xap/1.0/sType/Font#" xmlns:exif="http://ns.adobe.com/exif/1.0" xmlns:dc="http://purl.org/dc/elements/1.1/">\
+      <rdf:Description dc:creator="Kerstin Schwandtner" dc:date="2019-03-13 07:56:25 " dc:source="Camtasia,18.0.4,deu" dc:title="2019.03.12_LosCasos" dc:description="Videotutorial para el aprendizaje del concepto de casos" tscDM:firstFrame="2019.03.12_LosCasos_First_Frame.png" tscDM:keywords="Casos, alemán, argumentos, valencias, rasgos semántios" tscDM:originId="778830B7-E4BA-4EAF-84A5-E0752EF3FFAD" tscDM:project="2019.03.12_LosCasos">\
+         <xmpDM:duration xmpDM:scale="1/1000" xmpDM:value="560400"/>\
+         <xmpDM:videoFrameSize stDim:unit="pixel" stDim:h="788" stDim:w="1136"/>\
+         <tsc:langName>\
+            <rdf:Bag>\
+               <rdf:li xml:lang="de-DE">German</rdf:li></rdf:Bag>\
+         </tsc:langName>\
+         <xmpDM:Tracks>\
+            <rdf:Bag>\
+               <rdf:li>\
+                  <rdf:Description xmpDM:trackType="Quiz" xmpDM:frameRate="f1000" xmpDM:trackName="Quiz" tscIQ:quizGuid="7CD37AC2-FEC3-474A-9EC0-7953474089D9" tscIQ:authoredEmail="BW4DX21jScFwrbATi76yMvYldw9zlPponTmJ8/2P6DcdE+WRC3vsG7s9hHFYCM6uAW1IQsyO5GX5\
+H7s1Twm/BfKHDwIECfoMVovSLoO+oJkRax+Y0FMXLhAIfWdRcrFKuVTgBxIGzypu8NaISO5JWpbh\
+IS+JTM4ysMU7nsDipN6cMkhRwJgYi+QqjtRR1QSM62W71GL/84vp0CiYomjP1rhOhzOj589blZEv\
+lW2raeBv86+JI16UYQB1AMAxfv9Xz1Qy4MQCQWqt1M1+gLYWYAhvY+pLNF/AbKCNc2Ht0L3Bjq8J\
+STBVJ3JdgCyVm/gqKggYF27bFnFzGcfGAQ89/w==" tscIQ:requireUserId="0" tscIQ:locale="de-DE" tscIQ:reportMethod="SCORM" tscIQ:allowSkipQuiz="0" tscIQ:clientId="0E45EC2A-7CCA-4294-84CE-CB0980980952" tscIQ:hideReplay="0" tscIQ:quizHash="9523e5fc1d21af3be21134f9ef18241f">\
+                     <xmpDM:markers>\
+                        <rdf:Seq>\
+                           <rdf:li><rdf:Description xmpDM:startTime="126200" tscIQ:feedback="1" tscIQ:questionSetName="Quiz 7"><tscIQ:questions><rdf:Seq><rdf:li><rdf:Description tscIQ:type="MC" tscIQ:id="0"><tscIQ:question>¿Podrías indicar los casos que existen en la siguiente Frase? Die Mutter isst die Suppe. -&gt; La madre come la sopa</tscIQ:question><tscIQ:correctAnswer>1</tscIQ:correctAnswer><tscIQ:answerArray><rdf:Seq><rdf:li><rdf:Description tscIQ:orderId="0"><tscIQ:answer>Acusativo y nominativo</tscIQ:answer></rdf:Description></rdf:li><rdf:li><rdf:Description tscIQ:orderId="1"><tscIQ:answer>Dativo y nominativo</tscIQ:answer></rdf:Description></rdf:li><rdf:li><rdf:Description tscIQ:orderId="2"><tscIQ:answer>Acusativo y dativo</tscIQ:answer></rdf:Description></rdf:li></rdf:Seq></tscIQ:answerArray><tscIQ:feedback><rdf:Bag><rdf:li><rdf:Description tscIQ:reason="correct"><xmpDM:name><rdf:Alt><rdf:li>Correcto: Hay un nominativo (die Mutter) y un acusativo (die Suppe).</rdf:li></rdf:Alt></xmpDM:name></rdf:Description></rdf:li><rdf:li><rdf:Description tscIQ:reason="incorrect"><xmpDM:name><rdf:Alt><rdf:li>Hay un nominativo (die Mutter) y un acusativo (die Suppe).</rdf:li></rdf:Alt></xmpDM:name></rdf:Description></rdf:li></rdf:Bag></tscIQ:feedback></rdf:Description></rdf:li></rdf:Seq></tscIQ:questions></rdf:Description></rdf:li><rdf:li><rdf:Description xmpDM:startTime="248933" tscIQ:feedback="1" tscIQ:questionSetName="Quiz 8"><tscIQ:questions><rdf:Seq><rdf:li><rdf:Description tscIQ:type="MC" tscIQ:id="1"><tscIQ:question>¿Podrías indicar cuál es la valencia del verbo isst (come) de la siguiente frase? Die Kinder essen die Suppe. -&gt; Los niños comen la sopa:</tscIQ:question><tscIQ:correctAnswer>4</tscIQ:correctAnswer><tscIQ:answerArray><rdf:Seq><rdf:li><rdf:Description tscIQ:orderId="0"><tscIQ:answer>0</tscIQ:answer></rdf:Description></rdf:li><rdf:li><rdf:Description tscIQ:orderId="1"><tscIQ:answer>1</tscIQ:answer></rdf:Description></rdf:li><rdf:li><rdf:Description tscIQ:orderId="2"><tscIQ:answer>2</tscIQ:answer></rdf:Description></rdf:li><rdf:li><rdf:Description tscIQ:orderId="3"><tscIQ:answer>3</tscIQ:answer></rdf:Description></rdf:li></rdf:Seq></tscIQ:answerArray><tscIQ:feedback><rdf:Bag><rdf:li><rdf:Description tscIQ:reason="correct"><xmpDM:name><rdf:Alt><rdf:li>Correcto. Es un verbo bivalente.</rdf:li></rdf:Alt></xmpDM:name></rdf:Description></rdf:li><rdf:li><rdf:Description tscIQ:reason="incorrect"><xmpDM:name><rdf:Alt><rdf:li>Es un verbo bivalente. Necesita dos argumentos para tener un sentido comleto.</rdf:li></rdf:Alt></xmpDM:name></rdf:Description></rdf:li></rdf:Bag></tscIQ:feedback></rdf:Description></rdf:li></rdf:Seq></tscIQ:questions></rdf:Description></rdf:li><rdf:li><rdf:Description xmpDM:startTime="326767" tscIQ:feedback="1" tscIQ:questionSetName="Quiz 9"><tscIQ:questions><rdf:Seq><rdf:li><rdf:Description tscIQ:type="MC" tscIQ:id="2"><tscIQ:question>¿Podrías indicar cuales son los rasgos semánticos del argumento Dativo, die Frau, en la siguiente Frase? Der Mann gibt der Frau die Rose.</tscIQ:question><tscIQ:correctAnswer>1</tscIQ:correctAnswer><tscIQ:answerArray><rdf:Seq><rdf:li><rdf:Description tscIQ:orderId="0"><tscIQ:answer>+ animado + humano </tscIQ:answer></rdf:Description></rdf:li><rdf:li><rdf:Description tscIQ:orderId="1"><tscIQ:answer>+ animado  - humano</tscIQ:answer></rdf:Description></rdf:li><rdf:li><rdf:Description tscIQ:orderId="2"><tscIQ:answer>- animado + definido</tscIQ:answer></rdf:Description></rdf:li><rdf:li><rdf:Description tscIQ:orderId="3"><tscIQ:answer>- animado - definido</tscIQ:answer></rdf:Description></rdf:li></rdf:Seq></tscIQ:answerArray><tscIQ:feedback><rdf:Bag><rdf:li><rdf:Description tscIQ:reason="correct"><xmpDM:name><rdf:Alt><rdf:li>Correcto: + animado + humano </rdf:li></rdf:Alt></xmpDM:name></rdf:Description></rdf:li><rdf:li><rdf:Description tscIQ:reason="incorrect"><xmpDM:name><rdf:Alt><rdf:li>Die Frau tiene los siguientes rasgos semánticos: + animado + humano </rdf:li></rdf:Alt></xmpDM:name></rdf:Description></rdf:li></rdf:Bag></tscIQ:feedback></rdf:Description></rdf:li></rdf:Seq></tscIQ:questions></rdf:Description></rdf:li></rdf:Seq>\
+                     </xmpDM:markers>\
+                     <tscIQ:QuizParams><rdf:Bag><rdf:li xmpDM:name="txtPrev" xmpDM:value="anterior"/><rdf:li xmpDM:name="txtNext" xmpDM:value="siguiente"/><rdf:li xmpDM:name="txtAnswerQuestion" xmpDM:value="Responde a la siguiente pregunta"/><rdf:li xmpDM:name="txtSubmit" xmpDM:value="enviar respuesta"/><rdf:li xmpDM:name="txtReview" xmpDM:value="repetir"/><rdf:li xmpDM:name="txtReviewAnswer" xmpDM:value="mostrar respuestas"/><rdf:li xmpDM:name="txtContinue" xmpDM:value="seguir"/></rdf:Bag></tscIQ:QuizParams></rdf:Description>\
+               </rdf:li>\
+            </rdf:Bag>\
+         </xmpDM:Tracks>\
+         <tscDM:controller>\
+            <rdf:Description xmpDM:name="tscplayer">\
+               <tscDM:parameters>\
+                  <rdf:Bag>\
+                     <rdf:li xmpDM:name="autohide" xmpDM:value="true"/><rdf:li xmpDM:name="autoplay" xmpDM:value="false"/><rdf:li xmpDM:name="loop" xmpDM:value="false"/><rdf:li xmpDM:name="searchable" xmpDM:value="false"/><rdf:li xmpDM:name="captionsenabled" xmpDM:value="false"/><rdf:li xmpDM:name="sidebarenabled" xmpDM:value="false"/><rdf:li xmpDM:name="unicodeenabled" xmpDM:value="false"/><rdf:li xmpDM:name="backgroundcolor" xmpDM:value="000000"/><rdf:li xmpDM:name="sidebarlocation" xmpDM:value="left"/><rdf:li xmpDM:name="endaction" xmpDM:value="stop"/><rdf:li xmpDM:name="endactionparam" xmpDM:value="true"/><rdf:li xmpDM:name="locale" xmpDM:value="de-DE"/></rdf:Bag>\
+               </tscDM:parameters>\
+               <tscDM:controllerText>\
+                  <rdf:Bag>\
+                  </rdf:Bag>\
+               </tscDM:controllerText>\
+            </rdf:Description>\
+         </tscDM:controller>\
+         <tscDM:contentList>\
+            <rdf:Description>\
+               <tscDM:files>\
+                  <rdf:Seq>\
+                     <rdf:li xmpDM:name="0" xmpDM:value="2019.03.12_LosCasos.mp4"/><rdf:li xmpDM:name="1" xmpDM:value="2019.03.12_LosCasos.zip"/><rdf:li xmpDM:name="2" xmpDM:value="2019.03.12_LosCasos_First_Frame.png"/></rdf:Seq>\
+               </tscDM:files>\
+            </rdf:Description>\
+         </tscDM:contentList>\
+      </rdf:Description>\
+   </rdf:RDF>\
+</x:xmpmeta>';
